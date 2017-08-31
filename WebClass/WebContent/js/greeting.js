cdbed94793aa1e45699d919eb2104a3a2f6534d8
CJY <CJY@DESKTOP-CAJTDEF>
2017-08-31 10:34:37 +0900

    First

---
+++ WebClass/WebContent/js/greeting.js
@@ -0,0 +1,13 @@
+alert("안녕하세요?");
+function greeting(nation) {
+  if(nation == 'k') {
+    document.getElementById("result").innerHTML = "안녕하세요?";
+  }
+  if(nation == 'a') {
+    document.getElementById("result").innerHTML = "Hello";
+  }
+  if(nation == 'c') {
+    document.getElementById("result").innerHTML = "Ni hao";
+  }
+  console.log(5+6);
+}
